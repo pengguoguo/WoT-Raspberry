cbd605d308ac0f05ed9bcfaad4138869e456c1a0
pengguoguo <lzp20@163.com>
2019-08-02 15:59:44 +0800

    try use proxy function

--- wot-server.js
+++ wot-server.js
@@ -11,6 +11,6 @@ var server     = httpServer.listen(resources.pi.port,function () {
     console.info('WoT Pi is up and running on port %s',resources.pi.port);
 });
 
-export {ledproxy};
+modoule.exports = ledproxy;
 
 
