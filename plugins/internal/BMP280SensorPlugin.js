6f060dac38c8f2eaa44e72abb776e6525685e889
pengguoguo <lzp20@163.com>
2019-08-10 00:39:10 +0800

    finished leds and buzz control

---
+++ plugins/internal/BMP280SensorPlugin.js
@@ -0,0 +1,41 @@
+var resources = require('./../../resources/model');
+
+var pressureModel = resources.pi.sensors.pressure;
+
+var pressurePluginName = resources.pi.sensors.pressure.name;
+
+var pressurelocalParams;
+
+var bmp280Pressure;
+
+exports.start = function (params) {
+    pressurelocalParams = params;
+
+    connectHardware();
+};
+
+exports.stop = function(params) {
+    //sensor.unexport();
+};
+
+function connectHardware(){
+    var five  = require("johnny-five");
+    var raspi = require("raspi-io").RaspiIO;
+    var board = new five.Board({
+        io:raspi()
+    });
+};
+
+board.on("ready",function(){
+    bmp280Pressure = new five.IMU({
+        controller:'BMP280'
+    });
+
+
+});
+
+bmp280Pressure.on("data",function () {
+    console.info(this.altitude);
+    console.info(this.pressure);
+    console.info(this.temperature);
+});
--- wot-server.js
+++ wot-server.js
@@ -5,10 +5,13 @@ var resources  = require('./resources/model');
 //Internal plugins
 var ledsPlugin = require('./plugins/internal/ledsPlugin');
 var beepPlugin = require('./plugins/internal/beepPlugin');
+var bmp280PressurePlugin = require('./plugins/internal/BMP280SensorPlugin');
 
 ledsPlugin.start(1000);
 
 beepPlugin.start();
+
+bmp280PressurePlugin.start();
 
 var server     = httpServer.listen(resources.pi.port,function () {
     console.info('WoT Pi is up and running on port %s',resources.pi.port);
