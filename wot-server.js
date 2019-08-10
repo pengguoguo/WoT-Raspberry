be17f9a5c86da72ae12cf170401466b59a8384fb
pengguoguo <lzp20@163.com>
2019-08-11 00:29:27 +0800

    remove bme280 package

--- wot-server.js
+++ wot-server.js
@@ -3,13 +3,13 @@ var resources  = require('./resources/model');
 
 
 //Internal plugins
-var ledsPlugin = require('./plugins/internal/ledsPlugin');
-var beepPlugin = require('./plugins/internal/beepPlugin');
+//var ledsPlugin = require('./plugins/internal/ledsPlugin');
+//var beepPlugin = require('./plugins/internal/beepPlugin');
 var bmp180PressurePlugin = require('./plugins/internal/BMP180SensorPlugin');
 
-ledsPlugin.start(1000);
+//ledsPlugin.start(1000);
 
-beepPlugin.start();
+//beepPlugin.start();
 
 bmp180PressurePlugin.start();
 
