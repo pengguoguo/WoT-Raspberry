e248a803d7123da37bb124aa9d5356f4c27b9774
pengguoguo <lzp20@163.com>
2019-08-10 09:17:58 +0800

    added bmp280 controller function

--- plugins/internal/BMP280SensorPlugin.js
+++ plugins/internal/BMP280SensorPlugin.js
@@ -26,7 +26,7 @@ function connectHardware(){
 
     board.on('ready',function(){
         var bmp280Pressure = new five.Altimeter({
-            controller: "BMP280",
+            controller:"BMP280",
             elevation:12
         });
 
