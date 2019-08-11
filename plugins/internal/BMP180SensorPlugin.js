20527a89412900d54b926deaa417dcf8e08530c5
pengguoguo <lzp20@163.com>
2019-08-11 10:22:43 +0800

    remove raspio's BMP180 controller, because still error

--- plugins/internal/BMP180SensorPlugin.js
+++ plugins/internal/BMP180SensorPlugin.js
@@ -18,27 +18,4 @@ exports.stop = function(params) {
 
 function connectHardware(){
 
-    var five  = require('johnny-five');
-    var Raspi = require('raspi-io').RaspiIO;
-    var board = new five.Board({
-        io: new Raspi({
-            enableI2C:true
-        }),
-        repl: false
-    });
-
-    board.on('ready',function(){
-
-        console.info("board.on.ready");
-
-        var bmp180Multi = new five.Barometer({
-            controller: "BMP180",
-        });
-
-        bmp180Multi.on("changer",function(){
-            console.log("Barometer");
-            console.log("  pressure     : ", this.pressure);
-            console.log("--------------------------------------");
-        })
-    });
 };
